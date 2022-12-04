31bdbae3f961fa285a39377f55892057435a91cc
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-12-04 18:28:22 +0530

    Welcome to program

---
+++ src/com/bridgelabz/JdbcEmployeePayRollService.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcEmployeePayRollService {
+	public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/employee_payrolldata"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "Deepu765";
+        try {
+            Class.forName(
+                    "com.mysql.cj.jdbc.Driver"); // Driver name
+            Connection con = DriverManager.getConnection(
+                    url, username, password);
+            System.out.println(
+                    "Connection Established successfully");
+            con.close(); // close connection
+            System.out.println("Connection Closed....");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+}
